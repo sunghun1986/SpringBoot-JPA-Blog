e71d2e8553961efae5f244a7741a50f9558be600
csh <sunghun1986z@naver.com>
2020-10-20 19:19:43 +0900

    환경세팅완료

---
+++ src/test/java/com/csh/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csh.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
